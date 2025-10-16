4a43fc699d9a7e8688bbc44ec01d0e611c532200
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-10-16 19:50:20 +0200

    can do stuff

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
+import subprocess
+from tempfile import TemporaryDirectory
+
+import numpy as np
+from PIL import Image
+
+
 def main():
-    print("Hello from trex-effect!")
+    width = 512
+    height = 512
+    fps = 60
+    duration_seconds = 10
+
+    noise_data = np.random.randint(0, 256, (height, width), dtype=np.uint8)
+    noise_roll_data = noise_data.copy()
+
+    mask = np.array(Image.open("mask.png").convert("1"), dtype=bool)
+    with TemporaryDirectory() as temp_dir:
+        for i in range(fps * duration_seconds):
+            print(f"Generating frame {i}")
+            noise_roll_data = np.roll(noise_roll_data, shift=1, axis=0)
+
+            frame_data = noise_data.copy()
+            frame_data[mask] = noise_roll_data[mask]
+
+            frame_image = Image.fromarray(frame_data)
+            frame_image.save(f"{temp_dir}/frame_{i:04d}.png")
+
+        print("Generating video with ffmpeg...")
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-y",
+                "-framerate",
+                str(fps),
+                "-i",
+                f"{temp_dir}/frame_%04d.png",
+                "-c:v",
+                "libx264",
+                "-pix_fmt",
+                "yuv420p",
+                "output.mp4",
+            ]
+        )
 
 
 if __name__ == "__main__":
