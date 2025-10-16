b4425cfbed6ec1763f79e7553a7ba2cd8a9654e2
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-10-16 20:39:23 +0200

    animated bg, fix fg, color

--- main.py
+++ main.py
@@ -11,18 +11,23 @@ def main():
     fps = 60
     duration_seconds = 10
 
-    noise_data = np.random.randint(0, 256, (height, width), dtype=np.uint8)
-    noise_roll_data = noise_data.copy()
+    fg_data = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
+    bg_data = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
 
-    mask = np.array(Image.open("mask.png").convert("1"), dtype=bool)
+    mask = np.array(Image.open("mask.png").convert("L")) > 127
+
     with TemporaryDirectory() as temp_dir:
         for i in range(fps * duration_seconds):
             print(f"Generating frame {i}")
-            noise_roll_data = np.roll(noise_roll_data, shift=1, axis=0)
 
-            frame_data = noise_data.copy()
-            frame_data[mask] = noise_roll_data[mask]
+            # Move background to the left
+            bg_data = np.roll(bg_data, shift=-1, axis=1)
 
+            # Composite frame
+            frame_data = bg_data.copy()
+            frame_data[mask] = fg_data[mask]
+
+            # Save frame
             frame_image = Image.fromarray(frame_data)
             frame_image.save(f"{temp_dir}/frame_{i:04d}.png")
 
