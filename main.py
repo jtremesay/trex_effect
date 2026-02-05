1363c47db1e6708fa126d6c0b65982223d4f988d
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-02-05 19:12:52 +0100

    todo

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+INPUT_MOVIE_FILE = Path("data/rick_roll/original.webm")
+OUTPUT_MOVIE_FILE = Path("trex_effect.webm")
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
